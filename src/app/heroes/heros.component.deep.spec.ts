090547c77cd32c0979dbeb9bbaf700c8084cc43e
Amr Mohsen <amrmohsendev@gmail.com>
2024-03-25 08:07:03 +0200

    testing DOM interaction

--- src/app/heroes/heros.component.deep.spec.ts
+++ src/app/heroes/heros.component.deep.spec.ts
@@ -38,7 +38,7 @@ describe('HeroesComponent (deep tests)', () => {
   it('should render each hero as a HeroComponent', () => {
     mockHeroService.getHeroes.and.returnValue(of(HEROES));
     fixture.detectChanges(); // run ngOnInit
-    
+
     const heroComponentDEs = fixture.debugElement.queryAll(
       By.directive(HeroComponent)
     );
@@ -46,5 +46,49 @@ describe('HeroesComponent (deep tests)', () => {
     for (let i = 0; i < heroComponentDEs.length; i++) {
       expect(heroComponentDEs[i].componentInstance.hero).toEqual(HEROES[i]);
     }
+  });
+
+  it(`should call heroService.deleteHero when the Hero Component's delete button is clicked`, () => {
+    spyOn(fixture.componentInstance, 'delete');
+    mockHeroService.getHeroes.and.returnValue(of(HEROES));
+    fixture.detectChanges(); // run ngOnInit
+
+    const heroComponents = fixture.debugElement.queryAll(
+      By.directive(HeroComponent)
+    );
+
+    //call the delete method directly by triggering the click event
+    // heroComponents[0].query(By.css('button')).triggerEventHandler('click', {
+    //   stopPropagation: () => {},
+    // });
+
+    // or raise the delete event
+    // (<HeroComponent>heroComponents[0].componentInstance).delete.emit(undefined);
+
+    // or raise the delete event from a child component
+    heroComponents[0].triggerEventHandler('delete', null);
+    expect(fixture.componentInstance.delete).toHaveBeenCalledWith(HEROES[0]);
+  });
+
+  it('should add a new hero to the hero list when the add button is clicked', () => {
+    mockHeroService.getHeroes.and.returnValue(of(HEROES));
+    fixture.detectChanges(); // run ngOnInit
+
+    const name = 'Mr. Ice';
+    mockHeroService.addHero.and.returnValue(of({ id: 5, name, strength: 4 }));
+
+    const inputElement = fixture.debugElement.query(
+      By.css('input')
+    ).nativeElement; // get the input element
+    const addButton = fixture.debugElement.queryAll(By.css('button'))[0]; // get the add button
+
+    inputElement.value = name;
+    addButton.triggerEventHandler('click', null); // trigger the click event
+
+    fixture.detectChanges(); // update the view
+
+    const heroText = fixture.debugElement.query(By.css('ul')).nativeElement
+      .textContent;
+    expect(heroText).toContain('Ice');
   });
 });
